687c841f440902cf1419c6a8107a8de9a83d6468
JinnFighter <chernayamolnia@yandex.ru>
2019-11-16 15:30:10 +0400

    Added Game Controller and Bonfire states enum

---
+++ Assets/Scripts/Game Controller.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField] private Bonfire bonfire; //костер
+    [SerializeField] private Player player; //игрок
+    private float elapsedTime { get; set; }
+    private List <Stick> sticks;
+    private List<NPC> npcs;
+    private bool counting;
+
+    void Awake()
+    {
+        sticks = new List<Stick>();
+        npcs = new List<NPC>();
+    }
+    void Start()
+    {
+        elapsedTime = 0f;
+        counting = false;
+    }
+
+    void Update()
+    {
+        if(counting)
+            elapsedTime += Time.deltaTime;
+    }
+
+    void OnGameStart()
+    {
+        counting = true;
+    }
+    void OnGameOver()
+    {
+        counting = false;
+        //отправить результат на экран
+    }
+}
